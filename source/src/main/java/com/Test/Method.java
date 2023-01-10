35a89974e7a28b791aafd90786e493104045176d
Kevin Wan <kevin.wan@liferay.com>
2023-01-10 11:34:15 +0800

    An updated for master

--- source/src/main/java/com/Test/Method.java
+++ source/src/main/java/com/Test/Method.java
@@ -1,7 +1,7 @@
 package com.Test;
 
 public class Method {
-    public void aOutPutMethod(){
+    public void aOutPutMethod() {
         System.out.println("Hello World");
     }
 
@@ -10,11 +10,12 @@ public class Method {
         public static final String PORT = "8080";
     }
 
-    public static Object getUrl(){
+    public static Object getUrl() {
         String DOMAIN = "192.168.31.31";
         String PORT = "8080";
         String url = "http://" + DOMAIN + ":" + PORT;
         return url;
     }
+
     public static final String str = "abc123";
 }
--- source/src/main/java/com/junitTest/TestJunit.java
+++ source/src/main/java/com/junitTest/TestJunit.java
@@ -10,24 +10,46 @@ import static org.junit.Assert.assertEquals;
 
 public class TestJunit {
     @Test
-    public void canAssertResults(){
+    public void canAssertResults() {
         assertEquals("Return result is:", "abc123", str);
     }
 
     @Test
-    public void canAddTwoPlusTwo(){
-        int answer = 2+2;
-        assertEquals("2+2=4", 4, answer );
+    public void canAddTwoPlusTwo() {
+        int answer = 2 + 2;
+        assertEquals("2+2=4", 4, answer);
     }
 
     @Test
-    public void canOutputHelloWorldToConsole(){
+    public void canOutputHelloWorldToConsole() {
         Method myClass = new Method();
         myClass.aOutPutMethod();
     }
 
     @Test
-    public void canGetUrl(){
+    public void canConfirmIntMinAndMaxLimits() {
+
+        int minimumInt = -2147483648;
+        int maximumInt = 2147483647;
+
+        assertEquals("integer min", minimumInt, Integer.MIN_VALUE);
+        assertEquals("integer max", maximumInt, Integer.MAX_VALUE);
+    }
+
+    @Test
+    public void canConvertIntToHex() {
+        assertEquals("hex 11 is b", "b",
+                Integer.toHexString(11));
+        assertEquals("hex 10 is a", "a",
+                Integer.toHexString(10));
+        assertEquals("hex 3 is 3", "3",
+                Integer.toHexString(3));
+        assertEquals("hex 21 is 15", "15",
+                Integer.toHexString(21));
+    }
+
+    @Test
+    public void canGetUrl() {
 
         assertEquals("Returns Hard Coded URL",
                 "http://192.168.31.31:8080",
@@ -35,7 +57,7 @@ public class TestJunit {
     }
 
     @Test
-    public void canGetDomainAndPortStatically(){
+    public void canGetDomainAndPortStatically() {
 
         assertEquals("Just the Domain",
                 "192.168.31.31",
